4583188a78525fecb5ab3cba71ef5df8e531a616
vinhtrinhvan <trinhvanvinh018>
2022-08-09 14:45:13 +0700

    code kitty

--- kitty/lib.rs
+++ kitty/lib.rs
@@ -2,45 +2,72 @@
 
 use ink_lang as ink;
 
+// use ink_storage::traits::StorageLayout;
+
 #[ink::contract]
 mod kitty {
+    use ink_storage::{
+        traits::{SpreadAllocate, PackedLayout,SpreadLayout},
+        Mapping,
+    };
+    use ink_prelude::vec::Vec;
+    use scale::{Encode, Decode};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    #[derive(Debug, Clone, Encode, Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo, ::ink_storage::traits::StorageLayout))]
+    pub enum Gender{
+        Male, 
+        Female
+    }
+
+    #[derive(Debug, Clone, Encode, Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo, ::ink_storage::traits::StorageLayout))]
+    pub struct DrawKitty {
+        // Stores a single `bool` value on the storage.
+        dna: Vec<u8>,
+        price: u32,
+        gen_gender: Gender,
+        create_date: u64
+    }
+
     #[ink(storage)]
+    #[derive(Default, SpreadAllocate)]
     pub struct Kitty {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        owner_kitty: Mapping<AccountId, DrawKitty>,
+        
     }
 
     impl Kitty {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+        // Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+        
+            })
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn create_kitty(&mut self, _dna: Vec<u8>, _price: u32, _gen_gender: u8) -> Result<(),()> {
+            let caller = Self::env().caller();
+            let mut _gen = Gender::Female;
+            if _gen_gender == 1{
+                _gen = Gender::Male;
+            }
+            let _kitty = DrawKitty{
+                dna: _dna,
+                price: _price,
+                gen_gender: _gen,
+                create_date: Self::env().block_timestamp()
+            };
+            self.owner_kitty.insert(&caller, &_kitty);
+
+            Ok(())
         }
 
-        /// Simply returns the current value of our `bool`.
+        // /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn get_kitty(&self, accountId: AccountId) -> Option<DrawKitty>{
+            self.owner_kitty.get(accountId)
         }
     }
 
@@ -55,20 +82,20 @@ mod kitty {
         /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let kitty = Kitty::default();
-            assert_eq!(kitty.get(), false);
-        }
+        // We test if the default constructor does its job.
+        // #[ink::test]
+        // fn default_works() {
+        //     let kitty = Kitty::default();
+        //     assert_eq!(kitty.get(), false);
+        // }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut kitty = Kitty::new(false);
-            assert_eq!(kitty.get(), false);
-            kitty.flip();
-            assert_eq!(kitty.get(), true);
-        }
+        // /// We test a simple use case of our contract.
+        // #[ink::test]
+        // fn it_works() {
+        //     let mut kitty = Kitty::new(false);
+        //     assert_eq!(kitty.get(), false);
+        //     kitty.flip();
+        //     assert_eq!(kitty.get(), true);
+        // }
     }
 }
